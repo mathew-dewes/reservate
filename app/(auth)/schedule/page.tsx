3ec94055205a5d97e1474320e7ff79b438095416
Mathew Dewes <mathewdewes01@gmail.com>
2026-02-23 15:07:17 +1300

    Inter and Radley font added

--- app/(auth)/schedule/page.tsx
+++ app/(auth)/schedule/page.tsx
@@ -2,6 +2,10 @@ export default function schedulePage(){
     return (
         <div>
             <p>Schedule page</p>
+            <h1>Heading 1</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum explicabo debitis ex non eligendi minus quibusdam a illo ullam similique.</p>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
         </div>
     )
 }

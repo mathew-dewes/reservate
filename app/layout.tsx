a0565944fd4f6148470ab7e9c90d96c6615655a1
Mathew Dewes <mathewdewes01@gmail.com>
2026-02-25 09:54:45 +1300

    Styling added to navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import Navbar from "@/components/web/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 
 export const metadata: Metadata = {
@@ -15,13 +16,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={` antialiased max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8`}
-      >
-            <Navbar/>
-        <main className="mt-10">
+    <html suppressHydrationWarning lang="en">
+      <body className={`antialiased`}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange>
+  
+        <main className="mt-10 max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8">
+          <Navbar/>
         {children}
         </main>
+        </ThemeProvider>
+      
 
       </body>
     </html>
--- components/web/Navbar.tsx
+++ components/web/Navbar.tsx
@@ -1,17 +1,12 @@
-import Link from "next/link"
 
-export default function Navbar(){
+import DesktopNavigation from "./navigation/DesktopNavigation"
+
+export default function Navbar() {
     return (
         <div className="flex justify-between mt-10 items-center">
             <h1 className="font-bold text-xl">Reservate</h1>
 
-            <ul className="flex gap-10 items-center">
-                <Link href={'/schedule'}>Schedule</Link>
-                <Link href={'/explore'}>Explore</Link>
-                <Link href={'/bookings'}>Bookings</Link>
-                <Link href={'/account'}>Account</Link>
-  
-            </ul>
+          <DesktopNavigation/>
 
         </div>
     )
